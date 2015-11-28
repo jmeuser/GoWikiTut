0d32ce620e89775f7c243b11246fdb6baf65ea4a
jmeuser <jmeuser@icloud.com>
2015-11-28 16:41:36 -0600

    Introduced renderTemplate function to replace repeated code in view and edit handlers (still have to deal with possible errors from template.ParseFiles)

--- wiki.go
+++ wiki.go
@@ -25,11 +25,15 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w,p)
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,13 +42,12 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w,p)
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-//	http.HandleFunc("/save/", saveHandler)
+	//	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
